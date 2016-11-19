9dfa3cf45928dce8707d8fd23c041506ee03f57a
grantmw <grantmwong@gmail.com>
2016-11-19 15:59:31 -0800

    video list items create

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component { // React.component
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
+	render() {
+		return ( 
+			<div>
+				<input
+				value={this.state.term}
+				onChange={(event) => this.setState({ term: event.target.value })} />
+				Value of the input: {this.state.term}
+			</div>
+		);
+	}
+
+}
+
+export default SearchBar; //anything that imports search_bar, file will have comp. SearchBar; declare what to export
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+// Component to put on DOM
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar'; //path needed because it is not a library that is provided by npm
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyDv_OhlIj_Z6RHKayr59lHf6hbEMsWx1Zc';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			//this.setState({ videos: videos })
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
