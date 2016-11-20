4337efe221d812bb4e409cf2c52d30b2df30c8d8
grantmw <grantmwong@gmail.com>
2016-11-19 17:30:46 -0800

    embed video, css

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,12 +3,17 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
 	const videoItems = props.videos.map((video) => {
-		return <VideoListItem videos={video} />
+		return (
+			<VideoListItem 
+				onVideoSelect={props.onVideoSelect}
+				key={video.etag} 
+				video={video} />
+		);
 	}); //returns array
 
 	return (
 		<ul className="col-md-4 list-group">
-			{props.videos.length}
+			{videoItems}
 		</ul>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'; //path needed because it is not a library that is provided by npm
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDv_OhlIj_Z6RHKayr59lHf6hbEMsWx1Zc';
 
 
@@ -12,10 +13,16 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 
 		YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 			//this.setState({ videos: videos })
 		});
 	}
@@ -24,7 +31,10 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div>
 		);
 	}
